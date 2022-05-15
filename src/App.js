3849cf87f609ca90527f19b9d736d0901fda7861
ANS <36783010+answebdev@users.noreply.github.com>
2022-05-14 17:56:10 -0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import './App.css';
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
 
 // Video: https://www.youtube.com/watch?v=3HNyXCPDQ7Q
 // Code: https://github.com/adrianhajdin/project_professional_portfolio
 
 const App = () => {
-  return <h1>Sanity Portfolio</h1>;
+  return (
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;

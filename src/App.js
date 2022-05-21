f9cd0dbc9b6de8ca6ccba530c7c0d313c9ad9871
ANS <36783010+answebdev@users.noreply.github.com>
2022-05-21 15:34:51 -0700

    add animation hoc

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.scss';
 // Video: https://www.youtube.com/watch?v=3HNyXCPDQ7Q
 // Code: https://github.com/adrianhajdin/project_professional_portfolio
 
-// STOPPED AT: 3:17:45 => Next: Testimonial Section
+// STOPPED AT: 3:17:45 => Next: HOC / Testimonial Section
 
 const App = () => {
   return (
